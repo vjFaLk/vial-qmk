db8b3ad8dc280a252379a5de481ca7afdfec9029
Valmik J <mail@valmik.in>
2023-02-06 21:59:01 +0530

    add bento, crkbd rudrar and cirque

---
+++ keyboards/bento/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+#define VIAL_KEYBOARD_UID {0x18, 0x08, 0x8C, 0xF6, 0x08, 0x6A, 0x34, 0xC3}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
---
+++ keyboards/crkbd/keymaps/rudrar/rules.mk
@@ -0,0 +1,12 @@
+MOUSEKEY_ENABLE     = no      # Mouse keys
+RGBLIGHT_ENABLE     = yes     # Enable WS2812 RGB underlight.
+VIA_ENABLE          = yes     # Enable VIA
+VIAL_ENABLE         = yes     # Enable VIAL
+OLED_ENABLE         = yes
+OLED_DRIVER         = SSD1306
+EXTRAKEY_ENABLE     = yes
+LTO_ENABLE          = yes
+QMK_SETTINGS        = no
+COMBO_ENABLE        = no
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
