a2c63795697cb390a9190a2af9b5d3c792a1f4c9
Valmik J <mail@valmik.in>
2024-06-30 16:27:48 +0530

    add the_nonagon

---
+++ keyboards/the_nonagon/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xB4, 0xD7, 0x75, 0xF2, 0x52, 0x7F, 0x26, 0xF4}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
